5916657313a48246cb517002483ae880b594147e
fram <fram.dev.xyz@gmail.com>
2025-08-08 12:55:04 +0700

    Initial project

---
+++ src/main/java/com/xtramile/fram/FramApplication.java
@@ -0,0 +1,13 @@
+package com.xtramile.fram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xtramile/fram/FramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtramile.fram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
